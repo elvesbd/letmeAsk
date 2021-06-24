82192adb0871cb6ab9e238e3d60cc4204f591da3
elves brito <elvesbd@yahoo.com>
2021-06-24 12:24:01 -0300

    likes function, removing questions and closing the room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,6 +39,11 @@ export default function Home() {
       alert('Room does not exists');
       return;
     };
+
+    if (roomRef.val().endedAt) {
+      alert('Room already closed.')
+      return;
+    }
     history.push(`rooms/${roomCode}`)
   };
 
